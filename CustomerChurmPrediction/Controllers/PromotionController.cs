f633f8ab43603a132484ccb09d2019fd7873f537
arsenius <gamerpop25@gmail.com>
2024-11-17 17:45:03 +0400

    Add feature for working with promotions

--- CustomerChurmPrediction/Controllers/PromotionController.cs
+++ CustomerChurmPrediction/Controllers/PromotionController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CustomerChurmPrediction.Services;
+using CustomerChurmPrediction.Entities.PromotionEntity;
+using Microsoft.AspNetCore.Authorization;
+using MongoDB.Driver;
 
 namespace CustomerChurmPrediction.Controllers
 {
@@ -15,6 +18,124 @@ namespace CustomerChurmPrediction.Controllers
             _promotionService = promotionService;
             _logger = logger;
 
+        }
+
+        // Получить список рекламных постов
+        // GET: api/promotion
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetPromotionListAsync()
+        {
+            try
+            {
+                var filter = Builders<Promotion>.Filter.Empty;
+                var promotionList = await _promotionService.FindAllAsync(filter, default);
+
+                return Ok(new { promotionList = promotionList });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddPromotionAsync(PromotionAdd promotionAdd)
+        {
+            if (promotionAdd is null) 
+            {
+                return BadRequest();
+            }
+            try
+            {
+                Promotion promotion = new Promotion
+                {
+                    Title = promotionAdd.Title,
+                    Content = promotionAdd.Content,
+                    ImageUrl = promotionAdd.ImageUrl,
+                    LinkUrl = promotionAdd.LinkUrl,
+                    StartDate = promotionAdd.StartDate,
+                    EndDate = promotionAdd.EndDate
+                };
+
+                bool isSuccess = await _promotionService.SaveOrUpdateAsync(promotion);
+                if(isSuccess)
+                {
+                    return Ok(new { promotion = promotion });
+                }
+
+                // Возвращаю ошибку сервера
+                return StatusCode(500);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpPut("{promotionId}")]
+        public async Task<IActionResult> UpdatePromotionAsync(string promotionId, PromotionUpdate promotionUpdate)
+        {
+            if (string.IsNullOrEmpty(promotionId) || promotionUpdate is null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var promotion = await _promotionService.FindByIdAsync(promotionId, default);
+
+                if (promotion != null)
+                {
+                    promotion.Title = promotionUpdate.Title;
+                    promotion.Content = promotionUpdate.Content;
+                    promotion.ImageUrl = promotionUpdate.ImageUrl;
+                    promotion.LinkUrl = promotionUpdate.LinkUrl;
+                    promotion.StartDate = promotionUpdate.StartDate;
+                    promotion.EndDate = promotionUpdate.EndDate;
+
+                    bool isSuccess = await _promotionService.SaveOrUpdateAsync(promotion);
+                    if (isSuccess)
+                    {
+                        return Ok(new { promotion = promotion });
+                    }
+
+                    // Возвращаю ошибку сервера
+                    return StatusCode(500);
+                }
+
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpDelete]
+        [Route("{promotionId}")]
+        public async Task<IActionResult> DeletePromotionAsync(string promotionId)
+        {
+            if (string.IsNullOrEmpty(promotionId))
+                return BadRequest();
+            try
+            {
+                long deletedCount = await _promotionService.DeleteAsync(promotionId, default);
+                if (deletedCount > 0)
+                {
+                    return Ok(new { deletedCount = deletedCount });
+                }
+
+                // Если не удалено ни одной записи, возвращаю NotFound
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
--- CustomerChurmPrediction/Entities/PromotionEntity/PromotionAdd.cs
+++ CustomerChurmPrediction/Entities/PromotionEntity/PromotionAdd.cs
@@ -1,39 +1,38 @@
 ﻿namespace CustomerChurmPrediction.Entities.PromotionEntity
 {
-    // Реклама
-    public class Promotion : AbstractEntity
+    public class PromotionAdd
     {
         // Название поста.
-        public string Title {get; set;} = null!;
+        public string Title { get; set; } = null!;
         // Текстовое содержимое поста.
-        public string Content {get; set;} = null!;
+        public string Content { get; set; } = null!;
         // Ссылка на изображение (если пост содержит графику).
-        public string ImageUrl {get; set;} = null!;
+        public string ImageUrl { get; set; } = null!;
         // Ссылка, куда ведёт реклама(например, на товар или внешнюю страницу).
-        public string LinkUrl {get; set;} = null!;
+        public string LinkUrl { get; set; } = null!;
         // Дата и время начала публикации.
-        public string StartDate {get; set;} = null!;
+        public string StartDate { get; set; } = null!;
         // Дата и время начала публикации.
-        public string EndDate {get; set;} = null!;
+        public string EndDate { get; set; } = null!;
         // Логическое поле для определения активности поста.
-        public string IsActive {get; set;} = null!;
+        // public string IsActive { get; set; } = null!;
         // Целевая аудитория (например, демографические данные или категория пользователей).
-        public string TargetAudience {get; set;} = null!;
+        // public string TargetAudience { get; set; } = null!;
         // Теги для классификации поста(например, "распродажа", "новинка", "услуги").
-        public string Tags {get; set;} = null!;
+        // public string Tags { get; set; } = null!;
         // Идентификатор автора поста(сотрудник или администратор).
-        public string AuthorId {get; set;} = null!;
+        // public string AuthorId { get; set; } = null!;
         // Приоритет отображения(например, для управления сортировкой на сайте).
-        public string Priority {get; set;} = null!;
+        // public string Priority { get; set; } = null!;
         // Место отображения(например, "главная страница", "категория товаров", "страница блога").
-        public string Placement {get; set;} = null!;
+        // public string Placement { get; set; } = null!;
         // Место отображения(например, "главная страница", "категория товаров", "страница блога").
-        public string ClickCount {get; set;} = null!;
+        // public string ClickCount { get; set; } = null!;
         // Количество просмотров поста.
-        public string ViewCount {get; set;} = null!;
+        // public string ViewCount { get; set; } = null!;
         // Показатель кликабельности(Click-Through Rate) для аналитики.
-        public string CTR {get; set;} = null!;
+        // public string CTR { get; set; } = null!;
         // Логическое поле для закрепления поста в верхней части страницы.
-        public string IsPinned {get; set;} = null!;
+        //public string IsPinned { get; set; } = null!;
     }
 }
