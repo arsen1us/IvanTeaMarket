1f84dd3b4c08065c199f55eddab111dfaa02fccd
arsenius <gamerpop25@gmail.com>
2024-12-03 12:49:42 +0400

    fix

--- CustomerChurmPrediction/Entities/ProductEntity/ProductAdd.cs
+++ CustomerChurmPrediction/Entities/ProductEntity/ProductAdd.cs
@@ -2,10 +2,34 @@
 {
 	public class ProductAdd : AbstractEntity
 	{
-		public string Name { get; set; } = null!;
-		public string Description { get; set; } = null!;
-		public string CategoryId { get; set; } = null!;
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; } = null!;
+
+        /// <summary>
+        /// Id категории
+        /// </summary>
+        public string CategoryId { get; set; } = null!;
+
+        /// <summary>
+		/// Id компании
+		/// </summary>
 		public string CompanyId { get; set; } = null!;
+
+        /// <summary>
+		/// Цена
+		/// </summary>
 		public decimal Price { get; set; }
-	}
+
+        /// <summary>
+		/// Id фотографий
+		/// </summary>
+        public List<IFormFile>? ImageIds { get; set; }
+    }
 }
