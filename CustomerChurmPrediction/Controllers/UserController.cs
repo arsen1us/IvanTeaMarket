fc1dd4cce0ee47f81906d259244d58ee48eeb7a6
arsen1us <gamerpop25@gmail.com>
2025-03-19 12:05:22 +0400

    Добавил логирование в контроллер UserController

--- CustomerChurmPrediction/Controllers/UserController.cs
+++ CustomerChurmPrediction/Controllers/UserController.cs
@@ -23,22 +23,27 @@ namespace CustomerChurmPrediction.Controllers
         [Route("reg")]
         public async Task<IActionResult> RegisterAsync([FromBody] UserReg userReg)
         {
+            if (userReg is null
+                || string.IsNullOrEmpty(userReg.FirstName)
+                || string.IsNullOrEmpty(userReg.LastName)
+                || string.IsNullOrEmpty(userReg.Email)
+                || string.IsNullOrEmpty(userReg.Password))
+            {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(RegisterAsync)}] Переданный параметр [{nameof(userReg)}] равен null");
+                return BadRequest();
+            }
+
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1));
+            CancellationToken stoppingToken = cancellationTokenSource.Token;
+
             try
             {
-                if (userReg is null
-                    || string.IsNullOrEmpty(userReg.FirstName)
-                    || string.IsNullOrEmpty(userReg.LastName)
-                    || string.IsNullOrEmpty(userReg.Email)
-                    || string.IsNullOrEmpty(userReg.Password))
-                {
-                    return BadRequest();
-                }
-                User user = new User(userReg);
 
+                User user = new User(userReg);
                 // Выдаю роль - "Пользователь"
                 user.Role = UserRoles.User;
 
-                bool isSuccess = await _userService.SaveOrUpdateAsync(user, default);
+                bool isSuccess = await _userService.SaveOrUpdateAsync(user, stoppingToken);
 
                 if (isSuccess)
                 {
@@ -53,15 +58,17 @@ namespace CustomerChurmPrediction.Controllers
                         Expires = DateTimeOffset.UtcNow.AddHours(1)
                     });
 
+                    _logger.LogInformation($"[{DateTime.Now}] Метод [{nameof(RegisterAsync)}] Пользователь с id [{user.Id}] успешно зарегистрирован");
                     return Ok(new { token = token });
                 }
-                else
-                {
-                    return StatusCode(500);
-                }
+                
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(RegisterAsync)}] Не удалось зарегистрировать пользователя с id [{user.Id}]");
+                return StatusCode(500);
+                
             }
             catch (Exception ex)
             {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(RegisterAsync)}] Во время регистрации пользователя возникла ошибка. Детали ошибки: {ex.Message}");
                 return BadRequest(ex.Message);
             }
         }
@@ -74,22 +81,24 @@ namespace CustomerChurmPrediction.Controllers
         [Route("auth")]
         public async Task<IActionResult> AuthenticateAsync([FromBody] UserAuth userAuth)
         {
+            if (userAuth is null
+                || string.IsNullOrEmpty(userAuth.Email)
+                || string.IsNullOrEmpty(userAuth.Password))
+            {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(AuthenticateAsync)}] Переданный параметр [{nameof(userAuth)}] равен null");
+                return BadRequest();
+            }
+
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1));
+            CancellationToken stoppingToken = cancellationTokenSource.Token;
+
             try
             {
-                if (userAuth is null
-                    || string.IsNullOrEmpty(userAuth.Email)
-                    || string.IsNullOrEmpty(userAuth.Password))
-                {
-                    return BadRequest();
-                }
-                var user = await _userService.FindByEmailAndPassword(userAuth.Email, userAuth.Password, default);
+                var user = await _userService.FindByEmailAndPassword(userAuth.Email, userAuth.Password, stoppingToken);
 
                 if (user != null)
                 {
                     string token = _tokenService.GenerateJwtToken(user);
-
-                    // HttpContext.Response.Headers.Add("Authorization", token);
-                    // HttpContext.Response.Headers["Authorization"] = token;
 
                     string refreshToken = _tokenService.GenerateRefreshToken();
                     Response.Cookies.Append("RefreshToken", refreshToken, new CookieOptions
@@ -99,17 +108,17 @@ namespace CustomerChurmPrediction.Controllers
                         SameSite = SameSiteMode.Strict,
                         Expires = DateTimeOffset.UtcNow.AddHours(1)
                     });
-
+                    _logger.LogInformation($"[{DateTime.Now}] Метод [{nameof(AuthenticateAsync)}] Пользователь с id [{user.Id}] успешно прошёл аутентификацию");
                     return Ok(new { token = token });
                 }
-                else
-                {
-                    // Возвращаю Enternal Error (ошибка сервера)
-                    return StatusCode(500);
-                }
+
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(AuthenticateAsync)}] Не удалось аутентифицировать пользователя с id [{user.Id}]");
+                return StatusCode(500);
+                
             }
             catch (Exception ex)
             {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(AuthenticateAsync)}] Во время аутентификации пользователя возникла ошибка. Детали ошибки: {ex.Message}");
                 return BadRequest(ex.Message);
             }
         }
@@ -120,22 +129,34 @@ namespace CustomerChurmPrediction.Controllers
 
         [Authorize(Roles = "Admin, Owner, User")]
         [HttpGet("{userId}")]
-        public async Task<IActionResult> FindByIdAsync(string userId)
+        public async Task<IActionResult> FindUserByIdAsync(string userId)
         {
+            if (string.IsNullOrEmpty(userId))
+            {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(FindUserByIdAsync)}] Переданный параметр [{nameof(userId)}] равен null");
+                return BadRequest();
+            }
+
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1));
+            CancellationToken stoppingToken = cancellationTokenSource.Token;
+
             try
             {
-                if (string.IsNullOrEmpty(userId))
-                    return BadRequest();
 
-                User user = await _userService.FindByIdAsync(userId, default);
+                User user = await _userService.FindByIdAsync(userId, stoppingToken);
 
                 if (user is null)
+                {
+                    _logger.LogError($"[{DateTime.Now}] Метод [{nameof(FindUserByIdAsync)}] Пользователь с id [{userId}] не найден");
                     return NotFound();
+                }
 
+                _logger.LogInformation($"[{DateTime.Now}] Метод [{nameof(FindUserByIdAsync)}] Пользователь с id [{userId}] был успешно получен");
                 return Ok(new { user = user });
             }
             catch (Exception ex)
             {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(FindUserByIdAsync)}] Во время получения пользователя с id [{userId}] произошла ошибка. Детали ошибки: {ex.Message}");
                 return BadRequest(ex.Message);
             }
         }
@@ -147,41 +168,58 @@ namespace CustomerChurmPrediction.Controllers
 
         [Authorize(Roles = "Admin, Owner, User")]
         [HttpPut("{userId}")]
-        public async Task<IActionResult> UpdateAsync(string userId, [FromForm] UserUpdate userUpdate)
+        public async Task<IActionResult> UpdateUserAsync(string userId, [FromForm] UserUpdate userUpdate)
         {
             if (string.IsNullOrEmpty(userId) || userUpdate is null)
+            {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(UpdateUserAsync)}] Переданный параметр [{nameof(userId)}] или [{nameof(userUpdate)}] равен null");
                 return BadRequest();
+            }
+
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1));
+            CancellationToken stoppingToken = cancellationTokenSource.Token;
+
             try
             {
-                User user = await _userService.FindByIdAsync(userId, default);
+                User user = await _userService.FindByIdAsync(userId, stoppingToken);
 
                 if (user is null)
+                {
+                    _logger.LogError($"[{DateTime.Now}] Метод [{nameof(UpdateUserAsync)}] Пользователь с id [{userId}] не найден");
                     return NotFound();
+                }
 
-                user.FirstName = userUpdate.FirstName;
-                user.LastName = userUpdate.LastName;
-                user.Email = userUpdate.Email;
-                user.Password = userUpdate.Password;
+                user.FirstName = user.FirstName == userUpdate.FirstName ? user.FirstName : userUpdate.FirstName;
+                user.LastName = user.LastName == userUpdate.LastName ? user.LastName : userUpdate.LastName;
+                user.Email = user.Email == userUpdate.Email ? user.Email : userUpdate.Email;
+                user.Password = user.Password == userUpdate.Password ? user.Password : userUpdate.Password;
 
-                if (userUpdate.Images is null || userUpdate.Images.Count == 0)
-                    return BadRequest();
+                if (userUpdate.Images is not null)
+                {
+                    var userImageSrc = await _userService.UploadImagesAsync(userUpdate.Images, stoppingToken);
 
-                var userImageSrc = await _userService.UploadImagesAsync(userUpdate.Images, default);
+                    if (userImageSrc is null)
+                    {
+                        _logger.LogError($"[{DateTime.Now}] Метод [{nameof(UpdateUserAsync)}] Не удалось обновить аватар пользователя");
+                        return StatusCode(500);
+                    }
 
-                if (userImageSrc is null)
-                    return StatusCode(500);
+                    user.ImageSrcs = userImageSrc;
+                }
 
-                user.ImageSrcs = userImageSrc;
-
-                bool isSuccess = await _userService.SaveOrUpdateAsync(user, default);
-
+                bool isSuccess = await _userService.SaveOrUpdateAsync(user, stoppingToken);
                 if (isSuccess)
+                {
+                    _logger.LogInformation($"[{DateTime.Now}] Метод [{nameof(UpdateUserAsync)}] Информация о пользователе с id [{userId}] успешно обновлена");
                     return Ok(new { user = user });
+                }
 
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(UpdateUserAsync)}] Не удалось обновить информацию у пользователя с id [{userId}]");
                 return StatusCode(500);
             }
             catch (Exception ex)
             {
+                _logger.LogError($"[{DateTime.Now}] Метод [{nameof(UpdateUserAsync)}] Во время обновления информации у пользователя с id [{userId}] произошла ошибка. Детали ошибки: {ex.Message}");
                 return BadRequest(ex.Message);
             }
         }
